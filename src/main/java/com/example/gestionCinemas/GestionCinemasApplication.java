2c57ade643cea50b07147c31165bc2f019339447
gr7511 <seckgora@ept.sn>
2023-11-22 09:53:51 +0000

    beginning

---
+++ src/main/java/com/example/gestionCinemas/GestionCinemasApplication.java
@@ -0,0 +1,13 @@
+package com.example.gestionCinemas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionCinemasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionCinemasApplication.class, args);
+	}
+
+}
